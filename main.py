62410f9b88206f21f71a4bd0b37922538a3594e9
Aarya Jha <aj240502@gmail.com>
2022-03-27 03:42:18 +0530

    logic for pun display added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5.uic import loadUi
 import _thread
 import time
 import sys
+import random
 
 puns = [
     'What is a belt made of clocks called?\nWaist of time.',
@@ -18,23 +19,36 @@ class MainWindow(QDialog):
         super(MainWindow,self).__init__()
         print("main window initialized")
         loadUi("clock.ui",self)
+        self.label_1.setWordWrap(True)
+        self.label_1.setContentsMargins(10,10,10,10)
         QtCore.QTimer.singleShot(50, self.OnLoad)
 
     def OnLoad(self):
         _thread.start_new_thread(self.run_time,('time',0.001))
     
     def run_time(self, name, delay):
+        flag = 0 #to be used to ensure pun is only updated once at 5 seconds
         print("starting time thread")
+        self.label_1.setText(puns[random.randint(0,len(puns)-1)])
         while(1):
             t = time.localtime()
             current_time = time.strftime('%H:%M:%S',t)
             self.label.setText(current_time)
+            
+            if(t.tm_sec%10 == 0 and flag == 0):
+                print(t.tm_sec)
+                self.label_1.setText(puns[random.randint(0,len(puns)-1)])
+                flag = 1
+
+            if((t.tm_sec+1)%10 == 0 and flag == 1):
+                flag = 0
+
             time.sleep(delay)
 
 def main():
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
-    mainwindow.setWindowTitle("Clock")
+    mainwindow.setWindowTitle("Pun Clock")
     mainwindow.setWindowIcon(QtGui.QIcon('./images/icon.jpg'))
     mainwindow.show()
 
---
+++ readme.md
@@ -0,0 +1,13 @@
+<h2>The Pun Clock</h2>
+
+---
+
+<p align = right>Python, PyQt5</p>
+
+A simple clock in pyqt which updates and displays puns every 10 seconds. Implementation focus being -
+
+- multithreading, 
+- qt word-wrapping, 
+- window resizing, 
+- general design concepts for pyqt.
+
